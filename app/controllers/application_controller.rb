c418b2efd2c5ea158275ee4a00e5e13e659091b7
github username <github email address>
2018-03-24 13:56:38 -0500

    added application_controller w helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,18 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "buythedip"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end
 
 end

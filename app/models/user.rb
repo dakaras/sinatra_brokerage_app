d1d988f2ba655b998bbe2614857383473138119d
github username <github email address>
2018-03-24 13:47:44 -0500

    added all views for stock

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :username, :password, :email
   validates :username, uniqueness: true
+  validates_associated :accounts 
 end

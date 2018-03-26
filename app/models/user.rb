badf5e835f30622cdf06c00b1fbf1a58902bd27b
github username <github email address>
2018-03-25 19:54:11 -0500

    added password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :accounts
   has_secure_password
   validates_presence_of :username, :password, :email
+  validates :password, confirmation: true 
   validates :username, uniqueness: true
-  validates_associated :accounts 
+  validates_associated :accounts
 end

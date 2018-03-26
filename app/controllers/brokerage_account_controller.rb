de4657bf2de1ce97c1c19e92a33cdf33a24750ec
github username <github email address>
2018-03-25 19:02:06 -0500

    finishjed accounts controller

--- app/controllers/brokerage_account_controller.rb
+++ app/controllers/brokerage_account_controller.rb
@@ -1,3 +1,64 @@
+require 'rack-flash'
 class AccountsController < ApplicationController
 
-end
+  get '/accounts' do
+    if !logged_in
+      redirect '/login'
+    else
+      @accounts = Account.all
+      erb :'accounts/index'
+    end
+  end
+
+  get '/accounts/new' do
+    if !logged_in
+      redirect '/login'
+    else
+      erb :'accounts/new'
+    end
+  end
+
+  post '/accounts' do
+    if !logged_in
+      redirect '/login'
+    else
+      if params[:name] != "" && params[:category] != ""
+        Account.create(params)
+      else
+        flash[:message] = "All fields are required to create account."
+        erb :'accounts/new'
+      end
+    end
+
+    get '/accounts/:id/edit' do
+      if !logged_in
+        redirect '/login'
+      else
+        @account = Account.find_by(params[:id])
+        erb :'accounts/edit'
+      end
+    end
+
+    get '/accounts/:id' do
+      if !logged_in
+        redirect '/login'
+      else
+        @account = Account.find_by(params[:id])
+        erb :'accounts/show'
+      end
+    end
+
+    patch '/accounts/:id' do
+      @account = Account.find_by(params[:id])
+      if !logged_in
+        redirect '/login'
+      else
+        if params[:name] != "" && params[:category] != ""
+          Account.create(params)
+        else
+          erb :'accounts/edit'
+        end
+      end
+    end
+
+  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,14 @@ class UsersController < ApplicationController
     if !session[:user_id]
       erb :'users/create_user'
     else
-      redirect '/accounts'
+      redirect '/stocks'
     end
   end
 
   post '/signup' do
-    user = User.create(params)
-    if user.save
-      session[:id] = user.id
+    @user = User.create(params)
+    if @user.save
+      session[:id] = @user.id
       redirect '/accounts'
     else
       redirect '/signup'
@@ -28,10 +28,10 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    user = User.find_by(params[:username])
-    if user && user.authenticate(params[:password])
-      flash[:message] = "Welcome back, #{user.username}!"
-      session[:id] = user.id
+    @user = User.find_by(params[:username])
+    if @user && @user.authenticate(params[:password])
+      flash[:message] = "Welcome back, #{@user.username}!"
+      session[:id] = @user.id
       redirect '/accounts'
     elsif user
       flash[:message] = "Incorrect password. Please try again."
@@ -44,6 +44,7 @@ class UsersController < ApplicationController
 
   get '/users/:id' do
     if !logged_in?
+      flash[:message] = "Please Log In."
       redirect '/accounts'
     end
 
